89d5c6e79d964277aa37184e8362d932a9dec1a4
fsjavier <>
2023-06-30 11:46:33 +0000

    Add game page structure with basic style

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,11 @@ const howToPlayModal = document.getElementsByClassName("how-to-play-modal")[0];
 const openHowToPlayModal = document.getElementsByClassName("open-how-to-play")[0];
 const closeHowToPlayModal = document.getElementsByClassName("close-how-to-play")[0];
 
-openHowToPlayModal.addEventListener("click", () => {
+openHowToPlayModal.addEventListener("click", function () {
     howToPlayModal.showModal();
 });
 
-closeHowToPlayModal.addEventListener("click", () => {
+closeHowToPlayModal.addEventListener("click", function () {
     howToPlayModal.close();
 });
+
