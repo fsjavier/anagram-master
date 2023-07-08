c2385a2cc818eca518adf8d2e15189f71e45bb18
fsjavier <>
2023-07-08 18:11:03 +0000

    Add list of anagrams in the anagrams object

--- assets/js/script.js
+++ assets/js/script.js
@@ -256,19 +256,24 @@ const anagrams = {
             hint: "Very big mammal with big mouth and short legs"
         },
         {
-            name: "giraffe",
-            anagram: "fearfig",
-            hint: "Tallest living animal"
+            name: "leopard",
+            anagram: "parole",
+            hint: "Big cat known for its distinctive spots"
         },
         {
-            name: "elephant",
-            anagram: "thepanel",
-            hint: "Largest existing land animal"
+            name: "red panda",
+            anagram: "nap dared",
+            hint: "Arboreal mammal with a fluffy tail"
         },
         {
-            name: "hippopotamus",
-            anagram: "ahpompoustip",
-            hint: "Very big mammal with big mouth and short legs"
+            name: "racoon",
+            anagram: "corona",
+            hint: "Mammal often associated with urban environments and dumpster diving."
+        },
+        {
+            name: "horse",
+            anagram: "shore",
+            hint: "Powerful equine known for its strength and companionship with humans throughout history."
         }
     ],
     movies: [
@@ -290,7 +295,32 @@ const anagrams = {
         {
             name: "the dark knight",
             anagram: "thank third keg",
-            hint: "Redifined movies of superheros and villains"
+            hint: "Christopher Nolan redifined movies of superheros and villains"
+        },
+        {
+            name: "city lights",
+            anagram: "sigthly tic",
+            hint: "One of best Chaplin's movies"
+        },
+        {
+            name: "goodfellas",
+            anagram: "fooledgals",
+            hint: "Gripping crime masterpiece that follows the rise and fall of a charismatic mobster"
+        },
+        {
+            name: "star wars",
+            anagram: "raw arts",
+            hint: "The most famous battle between the light and dark sides"
+        },
+        {
+            name: "pulp fiction",
+            anagram: "licit funpop",
+            hint: "This interconnected stories made Tarantino one of the most popular directors"
+        },
+        {
+            name: "blade runner",
+            anagram: "burden learn",
+            hint: "Humanity and artificial intelligence mixed in a dystopian future"
         }
     ],
     music: [
@@ -298,6 +328,93 @@ const anagrams = {
             name: "linkin park",
             anagram: "plan kirk in",
             hint: "Created big hits like 'Numb' or 'In The End'"
+        },
+        {
+            name: "radiohead",
+            anagram: "hoardaide",
+            hint: "Their imeless hits like 'Creep', 'Karma Police', and 'Paranoid Android' have left an indelible mark on music history"
+        },
+        {
+            name: "the police",
+            anagram: "epoch lite",
+            hint: "Legendary rock band that defined the sound of the late '70s and early '80s creating unforgettable hits like 'Roxanne'"
+        },
+        {
+            name: "tina turner",
+            anagram: "nutria rent",
+            hint: "Queen of Rock 'n' Roll with hits like 'Simply the Best'"
+        },
+        {
+            name: "metallica",
+            anagram: "claimlate",
+            hint: "Heavy metal band with iconic hits like 'Enter Sandman' and 'Master of Puppets'"
+        },
+        {
+            name: "pink floid",
+            anagram: "kindly fop",
+            hint: "Rock band with timeless hits like 'Wish You Were Here' and 'Comfortably Numb' "
+        },
+        {
+            name: "the doors",
+            anagram: "dots hero",
+            hint: "Pioneering rock band that fused psychedelia, blues, and poetic lyrics"
+        },
+        {
+            name: "nirvana",
+            anagram: "rainvan",
+            hint: "groundbreaking grunge band that revolutionized the music scene"
+        },
+        {
+            name: "perturbing scenes",
+            anagram: "bruce springsteen",
+            hint: "Legendary rock artist 'Born to run' known for his poetic storytelling and passionate performances"
+        }
+    ],
+    countries: [
+        {
+            name: "portugal",
+            anagram: "polartug",
+            hint: "Located in southwestern Europe, known for its stunning coastline and vibrant culture."
+        },
+        {
+            name: "iceland",
+            anagram: "cleanid",
+            hint: "Land of breathtaking natural wonders, geothermal hot springs and the mesmerizing Northern Lights."
+        },
+        {
+            name: "vietnam",
+            anagram: "vanitem",
+            hint: "Southeast Asian nation with lush landscapes, bustling cities, and delicious cuisine."
+        },
+        {
+            name: "italy",
+            anagram: "layit",
+            hint: "European country with exquisite art, rich history, picturesque landscapes, and world-renowned cuisine"
+        },
+        {
+            name: "spain",
+            anagram: "pains",
+            hint: "Vibrant and passionate country, with lively festivals, mouthwatering cuisine, and sun-soaked beaches"
+        },
+        {
+            name: "argentina",
+            anagram: "giantearn",
+            hint: "Captivating South American nation celebrated for its passionate tango and breathtaking landscapes ranging from the Andes to the Pampas"
+        },
+        {
+            name: "germany",
+            anagram: "greyman",
+            hint: "European country that seamlessly combines rich history, cutting-edge technology and a deep appreciation for art, science, and engineering."
+        },
+        {
+            name: "colombia",
+            anagram: "claimboo",
+            hint: "Country of vibrant colors, diverse landscapes, rhythmic music, and rich cultural heritage, also knonw for its delicious coffee"
+        },
+        {
+            name: "united states",
+            anagram: "situated nest",
+            hint: "Vast and diverse nation that embodies the pursuit of freedom, opportunity, and innovation, with iconic landmarks and a captivating blend of natural wonders and urban marvels"
         }
     ]
 };
